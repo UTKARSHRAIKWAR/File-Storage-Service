2deb488935b05bd1c606e0cfa0f4b39bccd72f4a
UTKARSHRAIKWAR <utkarshraikwar555@gmail.com>
2025-09-27 00:17:17 +0530

    Auth

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = Schema({
   name: {
@@ -19,6 +20,20 @@ const userSchema = Schema({
     required: true,
   },
 });
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 
 const User = mongoose.model("User", userSchema);
 
